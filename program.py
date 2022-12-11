6f556591924779a590790a37a724eb875844e7a4
Admin <airstage@mail.ru>
2022-12-11 09:25:43 +0300

    added function with some print

--- program.py
+++ program.py
@@ -1,3 +1,6 @@
+def some_func():
+    print('New print!')
+
 def main():
     print('My first git program')
     print('And I change it every day')
@@ -5,6 +8,8 @@ def main():
     print('UFO came and added this line')
     print('And another one')
     print('And this one i added myself from github')
+    some_func()
+
 
 if __name__ == '__main__':
     main()

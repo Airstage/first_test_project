475cd9f837dbd679dae53289a49aaefedd37ed76
Admin <airstage@mail.ru>
2022-12-11 09:27:50 +0300

    added print without a function

--- program.py
+++ program.py
@@ -5,6 +5,7 @@ def main():
     print('UFO came and added this line')
     print('And another one')
     print('And this one i added myself from github')
+    print('And here is print without function')
 
 if __name__ == '__main__':
     main()
